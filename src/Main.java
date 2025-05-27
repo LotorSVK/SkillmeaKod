1fab068d22cde8d7e6eb354696ebc44e626afc75
LotorSVK <edwinkatona@gmail.com>
2025-05-27 19:29:25 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*String pozdrav = "Ahoj Svet";
+
+        // Toto je pre pisanie textu
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);*/
+
+        int prveCislo = 10;
+        int druheCislo =5;
 
 
-        System.out.println("Ahoj, Svet!");
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(prveCislo * druheCislo);
+        System.out.println(prveCislo / druheCislo);
 
+        String pozdrav2 = "Ahoj";
+        String jmeno = "Honza";
+
+        System.out.println(pozdrav2 + " " + jmeno);
 
 
     }
